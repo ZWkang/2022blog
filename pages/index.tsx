b5315c3047ba7f6ae477102b3c5459cbb615c54d
ZWkang <kang95630@gmail.com>
2021-08-20 23:43:42 +0800

    feat: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,77 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { getAllPost, getPost, Post } from '../utils/posts';
+import Header from '../components/header';
+import * as path from 'path';
+import pick from '../utils/pick';
+import Footer from '../components/footer';
 
-export default function Home() {
+interface IHomePageType {
+  posts: Array<
+    Partial<{
+      title: string;
+      date: string;
+      category: string[];
+      headImage: string;
+      abbr: string;
+      slug: string;
+      link: string;
+    }>
+  >;
+}
+
+function HomePage(props: IHomePageType) {
+  const { posts } = props;
   return (
-    <div className="antialiased">
-      <Head>
-        <title>欢迎来到 kk 的小站</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
+    <div className="flex flex-col h-screen justify-between">
+      <Header blogSiteName="zwkang"></Header>
+      <section className="mx-auto mb-auto mt-12 container max-w-xl">
         <header>
-          <h1>zwkang's blog</h1>
-          <nav>
-            <ul>
-              <li>
-                <a href="/home"></a>
-              </li>
-              <li>
-                <a href="/about"></a>
-              </li>
-              <li>
-                <a href="/rss"></a>
-              </li>
-            </ul>
-          </nav>
+          <h1 className="my-4 text-4xl logo">Post</h1>
         </header>
-      </div>
-
-      <div>
-        <main className={styles.main}>
-          <article>
-            <header>
-              <h1></h1>
-              <div>
-                <span>
-                  <time>时间</time>
-                </span>
-              </div>
-              <picture>
-                <img />
-              </picture>
-            </header>
-            <div>内容</div>
-            <footer></footer>
-          </article>
-        </main>
-      </div>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.title} className="my-1 text-gray-600">
+              <a
+                aria-label={post.abbr ?? post.title}
+                className="underline cursor-pointer inline-block transition-colors duration-200 hover:text-gray-900 font-medium"
+                href={post.link}
+              >
+                {post.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <hr className="mt-4 mb-4" />
+      <Footer />
     </div>
   );
+}
+
+export default HomePage;
+
+export async function getStaticProps() {
+  const allPosts = await getAllPost();
+  const postsResult = await Promise.all(
+    allPosts.map(async (p) => {
+      const result = await getPost(path.basename(p.path));
+      const link = `/posts/${path.basename(p.path)}`;
+      return {
+        ...result,
+        link,
+      };
+    })
+  );
+  const pickKeys = ['title', 'date', 'headImage', 'category', 'abbr', 'slug'];
+  const posts = postsResult.map((o) => {
+    return {
+      ...pick(o.data, pickKeys),
+      link: o.link,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
